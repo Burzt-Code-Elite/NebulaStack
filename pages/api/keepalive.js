774da3d997f9a5e70afe69b849e1f86e8133d1e2
Burzt-Code-Elite <143289431+Burzt-Code-Elite@users.noreply.github.com>
2025-09-18 06:53:22 +0530

    update keepalive

--- pages/api/keepalive.js
+++ pages/api/keepalive.js
@@ -5,7 +5,7 @@ const pool = new Pool({
 });
 
 export default async function handler(req, res) {
-  if (req.method !== 'GET') {
+  if (req.method !== 'GET' && req.method !== 'HEAD') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
